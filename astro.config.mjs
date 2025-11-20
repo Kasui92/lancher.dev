23b397268833cf14be1d178249f235924ed994d8
Luca Pattocchio <pattocchioluca@gmail.com>
2025-11-21 00:54:30 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import tailwindcss from "@tailwindcss/vite";
+import { remarkAlert } from "remark-github-blockquote-alert";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://lancher.dev",
+  base: "/",
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  },
+  markdown: {
+    remarkPlugins: [remarkAlert],
+  },
+});
